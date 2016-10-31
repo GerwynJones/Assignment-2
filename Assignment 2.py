ce7f5c8695ef9facf93ec03628984711c14c208c
Gezerj <JonesG71@cardiff.ac.uk>
2016-10-31 23:48:48 +0000

    Euler works ..... i think :vertical_traffic_light: -

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Mon Oct 31 23:17:23 2016
+
+@author: gezer
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Mon Oct 24 11:53:53 2016
 
 @author: C1331824
@@ -15,35 +22,38 @@ plt.ion() # needed for interactive plotting
 a = -1; b = 1
 Xc = np.array([a,b])
 Nx = 200
-Ti = 0; Tf = 2
+Ti = 0; Tf = 1.2
 T = np.array([Ti,Tf])
 c = 0.5
 
-def fu(y):
-    """ A function to call yprime and y """
-    return y
-
-def fup(y):
-    """ A function to call yprime and y """
-    return -.5*y
+def W1(Vi,dx,Nx):
+    Wt = np.zeros(len(Vi))
+    Wt = (1/2*dx)*(Vi - Vi)
+    return Wt
     
+def V1(Wi,dx,Nx):
+    Vt = np.zeros(len(Wi))
+    Vt = (1/2*dx)*(Wi - Wi)
+    return Vt
+    
+def U1(Vi,dx,Nx):
+    Ut = np.zeros(len(Vi))
+    Ut = Vi
+    return Ut
     
 def func(x, sigma):
-
     return np.exp(-(x**2)/(2*sigma**2))
     
-def Euler(Ui, Vi, fu, fup, dt):
+def Euler(Ui, Vi, Wi, W1,V1,U1, dx,Nx):
 
-    fU = fu(Vi)     # fy gives the value y of the function and fyp gives the value of yprime of the function
-    fUp = fup(Ui)
+    W = W1(Vi, dx,Nx)     # fy gives the value y of the function and fyp gives the value of yprime of the function
 
     # using euler method
-    U = Ui + dt*fU  # y value
-    Up = Vi + dt*fUp  # yprime value
-    return U, Up
+    y = yi + dt*fy  # y value
+    yp = ypi + dt*fyp  # yprime value
+    return y, yp
 
-def solver(Xc, Nx, T, c, method, func):
-    
+def solver(Xc,Nx,T,c,func):
     Ti, Tf = T
     a, b = Xc
     dx = (b-a)/Nx
@@ -52,7 +62,6 @@ def solver(Xc, Nx, T, c, method, func):
     x = np.linspace(a-dx,b,Nx+2)
     t = np.linspace(Ti,Tf,Nt+1) 
     U = np.zeros((Nx+2, Nt+1))
-    V = np.zeros((Nx+2, Nt+1))  
     
     #U[x,0]
     Xinit = func(x[1:Nx+1],0.1)
@@ -64,13 +73,13 @@ def solver(Xc, Nx, T, c, method, func):
     	
     for i in range(1,Nt+1):	 	
 
-        U[1:Nx+1,i],V[1:Nx+1,i] = method(U[0:Nx,i-1], V[0:Nx,i-1], fu, fup, dt)
+        U[1:Nx+1,i] = U[0:Nx,i-1] 
         U[0,i] = U[Nx,i]  
         U[Nx+1,i] = U[1,i]
 
-    return x, t, U
+    return x,t, U
 
-x, t, U = solver(Xc,Nx,T,c,Euler,func)
+x,t, U = solver(Xc,Nx,T,c,func)
 
 line1, = plt.plot(x, U[:,0], linewidth=4.0, color='r',label='re') 
 
@@ -80,3 +89,6 @@ for i in range(1,len(t)): # this steps through t values
 
 plt.ioff() 
 plt.show()
+
+figure()
+plt.plot(x, U[:,0])
