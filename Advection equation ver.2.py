ca55a9c5fd5cbfe5871061a9602c82ed5d1d3f3e
Gerwyn Jones <JonesG71@cardiff.ac.uk>
2016-11-01 18:27:54 +0000

    Working  Euler :smile:

--- Advection equation ver.2.py
+++ Advection equation ver.2.py
@@ -14,66 +14,66 @@ plt.ion() # needed for interactive plotting
 
 a = -1; b = 1
 Xc = np.array([a,b])
-Nx = 200
-Ti = 0; Tf = 1.2
+Nx = 50
+Ti = 0; Tf = 15
 T = np.array([Ti,Tf])
-c = 2
+c = 0.05
 
-def W1(Vi,dx,Nx):
-    Wt = np.zeros(len(Vi))
-    Wt[1:Nx+1] = (1/2*dx)*(Vi[2:Nx+2] - Vi[0:Nx])
-    return Wt
-    
-def V1(Wi,dx,Nx):
-    Vt = np.zeros(len(Wi))
-    Vt[1:Nx+1] = (1/2*dx)*(Wi[2:Nx+2] - Wi[0:Nx])
-    return Vt
-    
-def U1(Vi,dx,Nx):
-    Ut = np.zeros(len(Vi))
-    Ut[1:Nx+1] = Vi[1:Nx+1]
-    return Ut
+def U1(Ui, dx, Nx, v):
+
+    return  (v/(2*dx))*(Ui[2:Nx+2] - Ui[0:Nx])
+
+
+def Euler(Ui, U1, dt, dx, Nx, v):
+    """ The Y array has two values so we need to split them up 
+    when using euler"""
+    fU = U1(Ui, dx, Nx, v)
+
+    U = Ui[1:Nx+1] - dt*fU
+
+    return U
     
 def func(x, sigma):
     return np.exp(-(x**2)/(2*sigma**2))
 
-def solver(Xc,Nx,T,c,func):
+def solver(Xc, Nx, T, c, func, method):
     
     Ti, Tf = T
     a, b = Xc
     dx = (b-a)/Nx
-    dt = dx/c
+    dt = dx*c
     Nt = int((Tf-Ti)/dt)
     x = np.linspace(a-dx,b,Nx+2)
     t = np.linspace(Ti,Tf,Nt+1) 
     U = np.zeros((Nx+2, Nt+1))
+    V = np.zeros((Nx+2, Nt+1))
+    W = np.zeros((Nx+2, Nt+1))
     
     #U[x,0]
     Xinit = func(x[1:Nx+1],0.1)
     U[1:Nx+1,0] = Xinit
-    
+
     #boundary
     U[0,0] = U[Nx,0]
     U[Nx+1,0] = U[1,0]
     	
     for i in range(1,Nt+1):	 	
-
-        U[1:Nx+1,i] = U[0:Nx,i-1] 
+        """THIS CODE MUST CHANGE TO DO EULER AND RK4"""
+        U[1:Nx+1,i] = method(U[:,i-1], U1, dt, dx, Nx, c)
+       
         U[0,i] = U[Nx,i]  
         U[Nx+1,i] = U[1,i]
 
-    return x,t,U
+    return x, t, U
 
-x,t,U = solver(Xc,Nx,T,c,func)
+x, t, U = solver(Xc, Nx, T, c, func, Euler)
 
-line1, = plt.plot(x, U[:,0], linewidth=4.0, color='r',label='re') 
+line1, = plt.plot(x, U[:,0], linewidth=2.0, color='r',label='re') 
 
 for i in range(1,len(t)): # this steps through t values
     line1.set_ydata(U[:,i]) # changes the data for line1 
     plt.draw()
 
-plt.ioff() 
 plt.show()
+plt.ioff() 
 
-figure()
-plt.plot(x, U[:,0])
